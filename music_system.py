feb8accc34aba923ef247cff1f160322bdf0d5e6
Jisilver8 <78939957+Jisilver8@users.noreply.github.com>
2021-04-03 16:57:55 -0400

    Add music_system.py
    music playback subsystem

---
+++ music_system.py
@@ -0,0 +1,75 @@
+from pygame import mixer
+from generate_playlist import create_playlist
+import keyboard
+import time
+
+music_folder = "SpotifyMusic/"
+
+class MusicSystem:
+
+    def __init__(self, playlist):
+        self.playlist = [music_folder + i for i in playlist]
+        mixer.init()
+        self.pointer = 0
+        self.pause_flag = False
+
+    def play(self):
+        mixer.music.load(self.playlist[self.pointer])
+        mixer.music.play()
+
+    def pause(self):
+        mixer.music.pause()
+        self.pause_flag = True
+
+    def unpause(self):
+        mixer.music.unpause()
+        self.pause_flag = False
+
+    def next_song(self):
+        self.pointer += 1
+        if (self.pointer > len(playlist)):
+            self.pointer = 0
+        self.play()
+
+    def prev_song(self):
+        self.pointer -= 1
+        if (self.pointer < 0):
+            self.pointer = 0
+        self.play()
+
+    # play next song when finished
+    def check_busy(self):
+        if (not mixer.music.get_busy() and not self.pause_flag ):
+            self.next_song()
+
+    def run(self):
+        self.play()
+        print("Enter 'p' to pause, 'r' to resume, 'q' to skip, 'e' to go back ")
+        while True:
+            self.check_busy()
+
+            if keyboard.is_pressed('p'):
+                # Pausing the music
+                system.pause()
+                time.sleep(0.5)
+            elif keyboard.is_pressed('r'):
+                # Resuming the music
+                system.unpause()
+                time.sleep(0.5)
+            elif keyboard.is_pressed('q'):
+                system.next_song()
+                time.sleep(0.5)
+            elif keyboard.is_pressed('e'):
+                system.prev_song()
+                time.sleep(0.5)
+
+
+
+if __name__ == "__main__":
+    emotion = input("Please enter emotion: ")
+    emotion = emotion.lower()
+    playlist = create_playlist(emotion)
+    system = MusicSystem(playlist)
+    system.run()
+
+
